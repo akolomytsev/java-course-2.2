4b10e2dab3aa341973054a2623e1778891e29e66
Kolomytsev Alexander <kolm.87@mail.ru>
2021-02-05 23:51:12 +0600

    task6

--- JavaLevel2/src/Task6_Chat/Client_Chat.java
+++ JavaLevel2/src/Task6_Chat/Client_Chat.java
@@ -1,2 +1,57 @@
-package Task6_Chat;public class Client_Chat {
+package Task6_Chat;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client_Chat {
+
+    public static void main(String[] args) {
+
+        Socket socket = null;
+
+        try {
+            socket = new Socket("localhost", 3443);
+            // входящий поток
+            Scanner in =  new Scanner(socket.getInputStream());
+            // исходящий поток
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            // с консоли
+            Scanner sc = new Scanner(System.in);
+
+            // получаем в этом потоке информацию от сервера
+            Thread ran = new Thread(() -> {
+                while (true) {
+                        String msg = in.nextLine();
+                        System.out.println("Server: " + msg);
+                    }
+
+            });
+            ran.start();
+
+            // в главном потоке отправляем сообщение серверу
+            while (true) {
+
+                String msg = sc.nextLine();
+
+                System.out.println("From server: " + msg);
+                if (msg.equals("end")) {
+                    break;
+                }
+                // сообщение улетело серверу
+                out.println(msg);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+               // assert socket != null;
+
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- JavaLevel2/src/Task6_Chat/Server_Chat.java
+++ JavaLevel2/src/Task6_Chat/Server_Chat.java
@@ -1,2 +1,68 @@
-package Task6_Chat;public class Server_Chat {
+package Task6_Chat;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server_Chat {
+
+    public static void main(String[] args) {
+        //noinspection InstantiationOfUtilityClass
+        Server_Chat server = new Server_Chat();
+    }
+
+    public Server_Chat() {
+
+        // розетка сервера
+        ServerSocket server = null; // иницилизация локальной перемннной, так что пишу нолик
+        // hрозетка клиента, это некий поток, который будет подключаться к серверу
+        Socket socket = null;
+
+        try {
+            // порт, который будет прослушивать наш сервер
+            server = new ServerSocket(3443);
+            System.out.println("Server is working...");
+            socket = server.accept(); // клиент подключен
+            System.out.println("New Client");
+            // входящий поток
+            Scanner in =  new Scanner(socket.getInputStream());
+            // исходящий поток
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true); //или пишем внизу метод
+            // с консоли
+            Scanner sc = new Scanner(System.in);
+
+            // отправляем в этом потоке сообщение от сервера
+            Thread ran = new Thread(() -> {
+                while (true) {
+                        String msg = sc.nextLine();
+                        System.out.println("The message was send: " + msg);
+                        out.println(msg);
+                    }
+                });
+
+            ran.start();
+            // в главном потоке получаем сообщение и шлем эхо
+            while (true) {
+                String msg = in.nextLine();
+                /*if (msg.equals("/end")) {
+                    break;
+                }*/
+                System.out.println("Client: " + msg);
+                //               out.flush(); // это автоматический/принудительный сброс буфера вывода
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                //assert socket != null;
+                socket.close(); // закрываем клиента
+                server.close(); // закрываем клиента
+                System.out.println("Server closed");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
